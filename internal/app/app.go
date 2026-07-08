1803e62bb62b96b8334b6b822a585b9a377f9601
Erphun <s3niffer@Gmail.com>
2026-07-08 18:31:31 +0330

    Feat: json message into health check function

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,8 +1,9 @@
 package app
 
 import (
-	"encoding/json"
 	"net/http"
+
+	"github.com/s3niffer/taskmanagementapp/internal/utilities"
 )
 
 type Application struct {
@@ -13,9 +14,13 @@ func New() (*Application, error) {
 }
 
 func (Application) HealthCheck(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", ": application/json")
+	// w.Header().Add("Content-Type", ": application/json")
 
-	if err := json.NewEncoder(w).Encode("It looks fine. :)"); err != nil {
+	// if err := json.NewEncoder(w).Encode("It looks fine. :)"); err != nil {
+	// 	http.Error(w, "Something went wrong!.", http.StatusInternalServerError)
+	// }
+
+	if err := utilities.JsonMessage("It looks fine. :)", w); err != nil {
 		http.Error(w, "Something went wrong!.", http.StatusInternalServerError)
 	}
 }
