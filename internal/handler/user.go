e9e7ce2237aa61b46b4d9d7ba61676ec41ada18a
Erphun <s3niffer@Gmail.com>
2026-07-10 19:09:55 +0330

    Feat: guard to prevent duplicate username

--- internal/handler/user.go
+++ internal/handler/user.go
@@ -2,9 +2,12 @@ package handler
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"net/http"
 
 	"github.com/s3niffer/taskmanagementapp/internal/app"
+	"github.com/s3niffer/taskmanagementapp/internal/store"
 )
 
 type User struct {
@@ -25,7 +28,11 @@ func CreateUser(app app.Application, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if err := app.DB.User.CreateUser(user.Username, user.Password); err != nil {
+	if err := app.DB.User.CreateUser(user.Username, user.Password); errors.Is(store.UserDuplicateError, err) {
+		http.Error(w, fmt.Sprintf("already a user with username of %s exist.", user.Username), http.StatusBadRequest)
+		return
+	} else if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 }
--- internal/store/user.go
+++ internal/store/user.go
@@ -1,10 +1,22 @@
 package store
 
+import (
+	"errors"
+)
+
 type UserStore struct {
 	db *DataBase
 }
 
+var UserDuplicateError = errors.New("Duplicate username.")
+
 func (store *UserStore) CreateUser(u string, p string) error {
+
+	for _, v := range store.db.users {
+		if v.username == u {
+			return UserDuplicateError
+		}
+	}
 
 	store.db.users = append(store.db.users, user{username: u, password: p, id: store.db.modifiedTimes + 1})
 	store.db.modifiedTimes++
