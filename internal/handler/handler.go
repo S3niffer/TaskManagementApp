04d7a8e11851407de3d8d51ce76e3c64f7369d7c
Erphun <s3niffer@Gmail.com>
2026-07-13 00:35:21 +0330

    Add: chi router handler

---
+++ internal/handler/handler.go
@@ -0,0 +1,14 @@
+package handler
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/s3niffer/taskmanagementapp/internal/app"
+)
+
+func New(app app.Application) *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Get("/health", app.HealthCheck)
+
+	return r
+}
--- main.go
+++ main.go
@@ -6,19 +6,21 @@ import (
 	"time"
 
 	"github.com/s3niffer/taskmanagementapp/internal/app"
+	"github.com/s3niffer/taskmanagementapp/internal/handler"
 )
 
 func main() {
 	application := app.New()
+
+	handler := handler.New(application)
 
 	server := &http.Server{
 		Addr:         ":8080",
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 		IdleTimeout:  60 * time.Second,
+		Handler:      handler,
 	}
-
-	http.HandleFunc("/health", application.HealthCheck)
 
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatal(err)
