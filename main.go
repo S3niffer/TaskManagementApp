01792e97717a3e7bd3207a7761bacf0985f7b4ee
Erphun <s3niffer@Gmail.com>
2026-07-17 00:19:00 +0330

    Feat: use port from command flag

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -10,6 +12,10 @@ import (
 )
 
 func main() {
+	var port = 8080
+	flag.IntVar(&port, "port", 8080, "Server app port.")
+	flag.Parse()
+
 	application, err := app.New()
 	if err != nil {
 		log.Fatal(err)
@@ -19,12 +25,14 @@ func main() {
 	handler := handler.New(application)
 
 	server := &http.Server{
-		Addr:         ":8080",
+		Addr:         fmt.Sprintf(":%d", port),
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 		IdleTimeout:  60 * time.Second,
 		Handler:      handler,
 	}
+
+	fmt.Printf("\nServer in up on port: %d\n", port)
 
 	if err = server.ListenAndServe(); err != nil {
 		log.Fatal(err)
