a85fc7d6adaf0777e5affdcf2fb410b1828c0868
Erphun <s3niffer@Gmail.com>
2026-07-20 20:24:43 +0330

    Add: authMiddleware

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,12 +1,19 @@
 package handler
 
 import (
+	"fmt"
+	"net/http"
+	"strings"
+
 	"github.com/go-chi/chi/v5"
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/s3niffer/taskmanagementapp/internal/app"
 )
 
 func New(app app.Application) *chi.Mux {
 	r := chi.NewRouter()
+
+	r.Use(AuthMiddleware)
 
 	r.Get("/health", app.HealthCheck)
 
@@ -14,4 +21,29 @@ func New(app app.Application) *chi.Mux {
 	r.Post("/login", app.UserApi.LoginUser)
 
 	return r
+}
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tokenString := strings.TrimPrefix(r.Header.Get("Authorization"), "Bearer ")
+
+		token, err := jwt.Parse(
+			tokenString,
+			func(token *jwt.Token) (interface{}, error) {
+				return []byte("JWT_SECRET"), nil
+			},
+		)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+
+		claims := token.Claims.(jwt.MapClaims)
+
+		userId := claims["userID"]
+
+		fmt.Println(userId)
+
+		next.ServeHTTP(w, r)
+	})
 }
